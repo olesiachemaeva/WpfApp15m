70e770304b60d54e84288e3c16faf2da5fb2b0af
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2022-02-23 19:24:39 +1000

    Добавьте файлы проекта.

---
+++ WpfApp15m/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp15m
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Create))
+            {
+                if (docFDR.Document != null)
+                {
+                    XamlWriter.Save(docFDR.Document, fs);
+                    MessageBox.Show("Файл сохранен");
+                }
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Open))
+            {
+                FlowDocument document = XamlReader.Load(fs) as FlowDocument;
+                if (document != null)
+                    docFDR.Document = document;
+                //docViewer.Document = XamlReader.Load(fs) as FlowDocument;
+            }
+        }
+    }
+}
